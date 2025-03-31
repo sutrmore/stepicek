588d56c83492eaf0639ad38bdbd29074c8ce16bc
baljak98 <baljak98@gasos-ro.cz>
2025-03-31 11:27:28 +0200

    afafa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        boolean n = true;
+        List<String> favouriteBooks = new ArrayList<String>();
+        while(n==true){
+            System.out.print("Enter Book Name: ");
+            String bookName = sc.nextLine();
+            if(bookName.equals("konec")){
+                n = false;
+                for(int i=0; i<favouriteBooks.size(); i++){
+                    System.out.println(favouriteBooks.get(i));
+                }
+            }
+            else{
+                favouriteBooks.add(bookName);
+            }
+        }
     }
 }
